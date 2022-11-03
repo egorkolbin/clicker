8fe337fd89a86b38ff441453f310bd7f6344a1af
egorkolbin <88235382+egorkolbin@users.noreply.github.com>
2022-11-03 10:03:31 +0400

    Initial commit
    Generated by create-expo-app 1.1.4.

--- App.js
+++
@@ -1,9 +0,0 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Router from './router/Router';
-
-export default function App() {
-  return (
-    <Router/>
-  );
-}
